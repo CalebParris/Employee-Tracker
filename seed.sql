adcab9e3a52aeb5e8f05b3083dcbe1c2dff81587
Caleb Parris <calebparris@live.com>
2020-09-27 17:33:39 -0500

    Added required table columns to the seed.sql

--- seed.sql
+++ seed.sql
@@ -1,7 +1,7 @@
 USE employee_DB;
 
-INSERT INTO employee ()VALUES ();
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ();
 
-INSERT INTO role ()VALUES ();
+INSERT INTO role (title, salary, department_id) VALUES ();
 
-INSERT INTO department ()VALUES ();
+INSERT INTO department (name) VALUES ();
