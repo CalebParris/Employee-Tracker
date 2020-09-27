7acf7c26c9ac7f7d8852f2da62c5d2ffc007a6b0
Caleb Parris <calebparris@live.com>
2020-09-27 17:59:30 -0500

    Revert "Changed salary from decimal to int in the employeeSchema.sql"
    This reverts commit 090b4ca153c4469dfea1f55949269394ad357be9.

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employee(
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT;
     title VARCHAR(30) NOT NULL;
-    salary INT NOT NULL;
+    salary DECIMAL(10,4) NOT NULL;
     department_id INT NOT NULL;
     PRIMARY KEY (id);
 );
