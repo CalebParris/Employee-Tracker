06a8e7dd849474f8ecd2dddb32f83fb0e9a6f8da
Caleb Parris <calebparris@live.com>
2020-09-28 10:13:39 -0500

    Changed the rold_id and department_id to a default of NULL

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -7,7 +7,7 @@ CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT NULL,
     manager_id INT NULL,
     PRIMARY KEY (id),
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON UPDATE SET NULL ON DELETE SET NULL,
@@ -18,7 +18,7 @@ CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,4) NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT NULL,
     PRIMARY KEY (id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE SET NULL ON DELETE SET NULL
 );
