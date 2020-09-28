8e471ddb5e5504ac96ac09c5cc43de3626d7210b
Caleb Parris <calebparris@live.com>
2020-09-27 19:06:19 -0500

    Added foreign keys to reference the other tables

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -10,14 +10,17 @@ CREATE TABLE employee(
     role_id INT NOT NULL;
     manager_id INT NULL;
     PRIMARY KEY (id);
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON UPDATE SET NULL ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON UPDATE SET NULL ON DELETE SET NULL
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
     id INT NOT NULL AUTO_INCREMENT;
     title VARCHAR(30) NOT NULL;
     salary DECIMAL(10,4) NOT NULL;
     department_id INT NOT NULL;
     PRIMARY KEY (id);
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE SET NULL ON DELETE SET NULL
 );
 
 CREATE TABLE department(
