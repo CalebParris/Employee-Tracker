5f4ac6c2a2afbb6f02f7c219cec60ec488fdb8fd
Caleb Parris <calebparris@live.com>
2020-09-28 10:05:09 -0500

    Changed semi-colons to commas in the tables

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -4,29 +4,29 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE employee(
-    id INT NOT NULL AUTO_INCREMENT;
-    first_name VARCHAR(30) NOT NULL;
-    last_name VARCHAR(30) NOT NULL;
-    role_id INT NOT NULL;
-    manager_id INT NULL;
-    PRIMARY KEY (id);
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id),
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON UPDATE SET NULL ON DELETE SET NULL,
     CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON UPDATE SET NULL ON DELETE SET NULL
 );
 
 CREATE TABLE roles(
-    id INT NOT NULL AUTO_INCREMENT;
-    title VARCHAR(30) NOT NULL;
-    salary DECIMAL(10,4) NOT NULL;
-    department_id INT NOT NULL;
-    PRIMARY KEY (id);
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,4) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE SET NULL ON DELETE SET NULL
 );
 
 CREATE TABLE department(
-    id INT NOT NULL AUTO_INCREMENT;
-    name VARCHAR(30) NOT NULL;
-    PRIMARY KEY (id);
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 SELECT * FROM employee;
