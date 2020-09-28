bdbfac5ff5015045f07d8cb5a37f581b3ae7db73
Caleb Parris <calebparris@live.com>
2020-09-28 10:20:08 -0500

    Adjusted the Insert order and updated the table names

--- seed.sql
+++ seed.sql
@@ -1,9 +1,9 @@
 USE employee_DB;
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("John", "Smith", 1), ("Bob", "Johnson", 3), ("Jessica", "Wells", 5);
+INSERT INTO departments (name) VALUES ("Human Resources"), ("Software Engineering"), ("Payroll");
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jane", "Doe", 2, 1), ("Steve", "Jones", 4, 2), ("Zach", "Fatino", 6, 3);
+INSERT INTO roles (title, salary, department_id) VALUES ("HR Manager", 60000, 1), ("HR Representative", 40000, 1), ("Sr. Engineer", 120000, 2), ("Jr. Engineer", 60000, 2), ("Payroll Manager", 50000, 3), ("Payroll Associate", 40000, 3);
 
-INSERT INTO role (title, salary, department_id) VALUES ("HR Manager", 60000, 1), ("HR Representative", 40000, 1), ("Sr. Engineer", 120000, 2), ("Jr. Engineer", 60000, 2), ("Payroll Manager", 50000, 3), ("Payroll Associate", 40000, 3);
+INSERT INTO employees (first_name, last_name, role_id) VALUES ("John", "Smith", 1), ("Bob", "Johnson", 3), ("Jessica", "Wells", 5);
 
-INSERT INTO department (name) VALUES ("Human Resources"), ("Software Engineering"), ("Payroll");
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Jane", "Doe", 2, 1), ("Steve", "Jones", 4, 2), ("Zach", "Fatino", 6, 3);
