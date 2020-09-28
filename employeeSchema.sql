af000a2f8f565428a81b6be045b16e7758ff5d39
Caleb Parris <calebparris@live.com>
2020-09-28 10:21:55 -0500

    COrrected the table names

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE employee_DB;
 
 USE employee_DB;
 
-CREATE TABLE department(
+CREATE TABLE departments(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
@@ -18,7 +18,7 @@ CREATE TABLE roles(
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE SET NULL ON DELETE SET NULL
 );
 
-CREATE TABLE employee(
+CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
